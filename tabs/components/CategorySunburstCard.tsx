62b0757b4243757ff7d3950944442d6942012a6d
quantropi-minh <101128605+quantropi-minh@users.noreply.github.com>
2024-02-09 22:19:09 -0500

    refactors

--- tabs/components/CategorySunburstCard.tsx
+++ tabs/components/CategorySunburstCard.tsx
@@ -1,9 +1,11 @@
-import { Box, Paper, Typography, useTheme } from "@mui/material"
+import { Box, Button, ButtonBase, Card, CardContent, CardHeader, Chip, Paper, Typography, useTheme } from "@mui/material"
 import { ResponsiveSunburst } from "@nivo/sunburst"
-import { ComponentProps } from "react"
+import React, { ComponentProps } from "react"
 import { PlaidTransaction } from "~common/plaidTypes"
 import { SunburstNode, computeCategorySunburstData } from "./computeCategorySunburstData"
 import { dollarDisplay } from "~common/utils/displays"
+import { ChevronRight } from "@mui/icons-material"
+import { reverse, uniq } from "lodash"
 
 type SunburstNodeWithColor = SunburstNode & { color: string, children: SunburstNodeWithColor []}
 
@@ -24,7 +26,7 @@ const colorByCategory = async (category: string) => {
   ];
 }
 
-export const CategorySunburst = (props: { 
+const CategorySunburst = (props: { 
   transactions: PlaidTransaction[], 
   onClick?: ComponentProps<typeof ResponsiveSunburst>['onClick'] 
   rootPath?: string[]
@@ -72,5 +74,46 @@ export const CategorySunburst = (props: {
       />
     </Box>
 
+  )
+}
+
+export const CategorySunburstCard = (props: {
+  categoryFilter: string[],
+  setCategoryFilter: (filter: typeof props.categoryFilter) => any
+  spendings: PlaidTransaction[]
+}) => {
+  const { categoryFilter, setCategoryFilter, spendings } = props
+  return (
+    <Card variant="outlined" sx={{ width: "100%", minHeight: "100%" }}>
+      <CardHeader title="Categories" subheader={
+        <Box display={"flex"} flexDirection={"row"} alignItems={"center"} flexWrap={"wrap"} mt={1}>
+          {!!categoryFilter.length
+            ? (
+              <>
+                <Button size="small" onClick={() => setCategoryFilter([])} >Clear</Button>
+                {
+                  categoryFilter.map((c, i) => (
+                    <React.Fragment key={`${c}-${i}`}>
+                      {i !== 0 && <ChevronRight />}
+                      <Chip
+                        label={c} size="small"
+                        component={ButtonBase}
+                        onClick={() => setCategoryFilter(categoryFilter.slice(0, i + 1))}
+                      />
+                    </React.Fragment>
+                  ))
+                }
+              </>
+            )
+            : "All"}
+        </Box>
+      } />
+      <CardContent sx={{ height: "400px" }}>
+        <CategorySunburst rootPath={categoryFilter} transactions={spendings} onClick={({ path }) => {
+          const filters = reverse(path.filter(p => p !== "root") as string[])
+          setCategoryFilter(uniq([...categoryFilter, ...filters]))
+        }} />
+      </CardContent>
+    </Card>
   )
 }
